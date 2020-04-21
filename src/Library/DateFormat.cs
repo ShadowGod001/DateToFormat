45e5d0f39cd67e763a255ee6654a850aaf785fa0
ShadowGod001 <55005167+ShadowGod001@users.noreply.github.com>
2020-04-21 00:55:34 -0300

    Tercer Intento
    Se agregan nuevos métodos y nuevos tests

--- src/Library/DateFormat.cs
+++ src/Library/DateFormat.cs
@@ -4,21 +4,65 @@ namespace Library
 {   
 
     public class DateFormat
-    {
+    {   
+
+        // Se cambia el formato de la fecha invirtiendo la misma y agregando guiones.
+
         public static string DateToOneFormat(string cadena) 
-        {
-            string fecha = cadena.Replace("/","-");
-            return fecha.Substring(6)+fecha.Substring(2,4)+fecha.Substring(0,2); 
+        {   
+            string fecha = cadena.Substring(6)+ "-" +cadena.Substring(3,2)+ "-" +cadena.Substring(0,2);
+            if (VerifyDigits(fecha) || VerifyLength(fecha))
+            {
+                fecha = "00-00-00";
+            }
+            return fecha; 
         }   
+
+        // Se cambia el formato de la fecha a la misma con guiones.
 
         public static string DateToAnotherFormat(string cadena) 
-        {
-            string fecha = cadena.Replace("/","-");
+        {   
+            string fecha = cadena.Substring(0,2)+ "-" +cadena.Substring(3,2)+ "-" +cadena.Substring(6);
+            if (VerifyDigits(fecha) || VerifyLength(fecha))
+            {
+                fecha = "00-00-00";
+            }
             return fecha;
-        }   
+        }
+
+        // Se verifica que el largo de la cadena sea correcto.
+
+        public static bool VerifyLength(string cadena)
+        {
+            return cadena.Length!=10;
+        }  
+
+        // Se verifica que la cadena tenga la cantidad correcta de numeros y guiones.
+
+        public static bool VerifyDigits(string cadena) 
+        {
+            int contadorNum = 0;
+            int contadorGuion = 0;
+
+            foreach (var item in cadena)
+            {
+                if (char.IsNumber(item))
+                {
+                    contadorNum += 1;
+                }
+                else if (item.ToString() == "-")
+                {
+                    contadorGuion += 1;
+                }
+            }
+
+            return !(contadorNum == 8 && contadorGuion == 2);
 
 
+        }
 
-    }
+
     
+    }
+
 }
--- src/Test/Library.Test/UnitTest1.cs
+++ src/Test/Library.Test/UnitTest1.cs
@@ -32,5 +32,53 @@ namespace Library.Test
 
             Assert.AreEqual(prueba,Library.DateFormat.DateToAnotherFormat(original));
         }
+
+        [Test]
+
+        // Test 3 con TDD.
+        
+         public void DateToOneFormat_WrongDate_DateWithZeros()  
+        {   
+            string original = "10/11/1977454";
+            string prueba = "00-00-00";
+
+            Assert.AreEqual(prueba,Library.DateFormat.DateToOneFormat(original));
+        }
+
+        [Test]
+
+        // Test 4 con TDD.
+        
+         public void DateToAnotherFormat_WrongDate_DateWithZeros()  
+        {   
+            string original = "10/11/1977454";
+            string prueba = "00-00-00";
+
+            Assert.AreEqual(prueba,Library.DateFormat.DateToAnotherFormat(original));
+        }
+
+        [Test]
+
+        // Test 5 con TDD.
+        
+         public void DateToOneFormat_DateWithIncorrectDigits_DateWithZeros()  
+        {   
+            string original = "1*011/1977";
+            string prueba = "00-00-00";
+
+            Assert.AreEqual(prueba,Library.DateFormat.DateToOneFormat(original));
+        }
+
+         [Test]
+
+        // Test 6 con TDD.
+          public void DateToOneFormat_DateWithNumbersAndLetters_DateWithZeros()  
+        {   
+            string original = "10A/11/1977";
+            string prueba = "00-00-00";
+
+            Assert.AreEqual(prueba,Library.DateFormat.DateToOneFormat(original));
+        }
+
     }
 }
